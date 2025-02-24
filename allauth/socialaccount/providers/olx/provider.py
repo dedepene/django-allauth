1e8383fc1349bf5cb6930b2fb3483674492cbdf3
dedepene <rvelitchkov@gmail.com>
2025-02-24 21:08:17 +0200

    Add OLX provider to social apps
    fix to provider.py
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/dedepene/django-allauth?shareId=XXXX-XXXX-XXXX-XXXX).

--- allauth/socialaccount/providers/olx/provider.py
+++ allauth/socialaccount/providers/olx/provider.py
@@ -2,6 +2,7 @@ from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
 from allauth.socialaccount.models import SocialAccount, SocialLogin, SocialToken
 from allauth.account.models import EmailAddress
 from allauth.socialaccount.providers.olx.views import OLXOAuth2Adapter
+from allauth.socialaccount.providers.base.provider import ProviderAccount
 
 class OlxAccount(ProviderAccount):
     pass
